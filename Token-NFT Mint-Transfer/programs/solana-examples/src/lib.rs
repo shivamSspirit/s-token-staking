b6a4d801c570068692ba5b7fd7465b1a55ae1e9e
memxor <bhattacharyakun@gmail.com>
2024-01-14 15:36:24 +0530

    Organizing

--- Token-NFT Mint-Transfer/programs/solana-examples/src/lib.rs
+++ Token-NFT Mint-Transfer/programs/solana-examples/src/lib.rs
@@ -1,7 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{ associated_token::AssociatedToken, token::{self, Transfer}, metadata::
-    {create_master_edition_v3, create_metadata_accounts_v3, CreateMasterEditionV3, CreateMetadataAccountsV3, Metadata}, token::{mint_to, Mint, MintTo, Token, TokenAccount }};
-use mpl_token_metadata::{ pda::{ find_master_edition_account, find_metadata_account}, state::DataV2 };
+pub mod instructions;
+pub use instructions::*;
 
 declare_id!("FbZAgbRfqxAx27JYaFuBqaYieBWYoDKkkfnD8pFJriVn");
 
@@ -12,207 +11,16 @@ pub mod solana_examples
 
     pub fn mint_nft(ctx: Context<MintNFT>, name: String, symbol: String, uri: String) -> Result<()> 
     {
-        let cpi_context = CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            MintTo {
-                mint: ctx.accounts.mint.to_account_info(),
-                to: ctx.accounts.associated_token_account.to_account_info(),
-                authority: ctx.accounts.signer.to_account_info(),
-            },
-        );
-
-        mint_to(cpi_context, 1)?;
-
-        let cpi_context = CpiContext::new(
-            ctx.accounts.token_metadata_program.to_account_info(),
-            CreateMetadataAccountsV3 {
-                metadata: ctx.accounts.metadata_account.to_account_info(),
-                mint: ctx.accounts.mint.to_account_info(),
-                mint_authority: ctx.accounts.signer.to_account_info(),
-                update_authority: ctx.accounts.signer.to_account_info(),
-                payer: ctx.accounts.signer.to_account_info(),
-                system_program: ctx.accounts.system_program.to_account_info(),
-                rent: ctx.accounts.rent.to_account_info(),
-            },
-        );
-
-        let data_v2 = DataV2 {
-            name,
-            symbol,
-            uri,
-            seller_fee_basis_points: 0,
-            creators: None,
-            collection: None,
-            uses: None,
-        };
-
-        create_metadata_accounts_v3(cpi_context, data_v2, false, true, None)?;
-
-        let cpi_context = CpiContext::new(
-            ctx.accounts.token_metadata_program.to_account_info(),
-            CreateMasterEditionV3 {
-                edition: ctx.accounts.master_edition_account.to_account_info(),
-                mint: ctx.accounts.mint.to_account_info(),
-                update_authority: ctx.accounts.signer.to_account_info(),
-                mint_authority: ctx.accounts.signer.to_account_info(),
-                payer: ctx.accounts.signer.to_account_info(),
-                metadata: ctx.accounts.metadata_account.to_account_info(),
-                token_program: ctx.accounts.token_program.to_account_info(),
-                system_program: ctx.accounts.system_program.to_account_info(),
-                rent: ctx.accounts.rent.to_account_info(),
-            },
-        );
-
-        create_master_edition_v3(cpi_context, None)?;
-
-        Ok(())
+        mint_nft::mint_nft(ctx, name, symbol, uri)
     }
 
-    pub fn mint_token(ctx: Context<MintToken>, _decimals: u8, name: String, symbol: String, uri: String, amount: u64) -> Result<()> 
+    pub fn mint_token(ctx: Context<MintToken>, decimals: u8, name: String, symbol: String, uri: String, amount: u64) -> Result<()> 
     {
-        let cpi_context = CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            MintTo {
-                mint: ctx.accounts.mint.to_account_info(),
-                to: ctx.accounts.associated_token_account.to_account_info(),
-                authority: ctx.accounts.signer.to_account_info(),
-            },
-        );
-
-        mint_to(cpi_context, amount)?;
-
-        let cpi_context = CpiContext::new(
-            ctx.accounts.token_metadata_program.to_account_info(),
-            CreateMetadataAccountsV3 {
-                metadata: ctx.accounts.metadata_account.to_account_info(),
-                mint: ctx.accounts.mint.to_account_info(),
-                mint_authority: ctx.accounts.signer.to_account_info(),
-                update_authority: ctx.accounts.signer.to_account_info(),
-                payer: ctx.accounts.signer.to_account_info(),
-                system_program: ctx.accounts.system_program.to_account_info(),
-                rent: ctx.accounts.rent.to_account_info(),
-            },
-        );
-
-        let data_v2 = DataV2 {
-            name,
-            symbol,
-            uri,
-            seller_fee_basis_points: 0,
-            creators: None,
-            collection: None,
-            uses: None,
-        };
-
-        create_metadata_accounts_v3(cpi_context, data_v2, false, true, None)?;
-
-        Ok(())
+        mint_token::mint_token(ctx, decimals, name, symbol, uri, amount)
     }
 
     pub fn transfer_tokens(ctx: Context<TransferToken>, amount: u64) -> Result<()> 
     {
-        let cpi_accounts = Transfer 
-        {
-            from: ctx.accounts.from_ata.to_account_info(),
-            to: ctx.accounts.to_ata.to_account_info(),
-            authority: ctx.accounts.from.to_account_info(),
-        };
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        
-        token::transfer(CpiContext::new(cpi_program, cpi_accounts), amount)?;
-
-        Ok(())
+        transfer_token::transfer_tokens(ctx, amount)
     }
-}
-
-#[derive(Accounts)]
-pub struct MintNFT<'info> 
-{
-    /// CHECK: signer check
-    #[account(mut, signer)]
-    signer: AccountInfo<'info>,
-
-    #[account(
-        init,
-        payer = signer,
-        mint::decimals = 0,
-        mint::authority = signer.key(),
-        mint::freeze_authority = signer.key()
-    )]
-    mint: Account<'info, Mint>,
-
-    #[account(
-        init_if_needed,
-        payer = signer,
-        associated_token::mint = mint,
-        associated_token::authority = signer
-    )]
-    pub associated_token_account: Account<'info, TokenAccount>,
-
-    /// CHECK:
-    #[account(mut, address = find_metadata_account(&mint.key()).0)]
-    pub metadata_account: AccountInfo<'info>,
-
-    /// CHECK:
-    #[account(mut, address = find_master_edition_account(&mint.key()).0)]
-    pub master_edition_account: AccountInfo<'info>,
-
-    pub token_program: Program<'info, Token>,
-    pub rent: Sysvar<'info, Rent>,
-    pub associated_token_program: Program<'info, AssociatedToken>,
-    pub token_metadata_program: Program<'info, Metadata>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-#[instruction(decimals: u8)]
-pub struct MintToken<'info> 
-{
-    /// CHECK: signer check
-    #[account(mut, signer)]
-    signer: AccountInfo<'info>,
-
-    #[account(
-        init,
-        payer = signer,
-        mint::decimals = decimals,
-        mint::authority = signer.key(),
-        mint::freeze_authority = signer.key(),
-    )]
-    mint: Account<'info, Mint>,
-
-    /// CHECK:
-    #[account(mut, address = find_metadata_account(&mint.key()).0)]
-    pub metadata_account: AccountInfo<'info>,
-
-    #[account(
-        init_if_needed,
-        payer = signer,
-        associated_token::mint = mint,
-        associated_token::authority = signer,
-    )]
-    pub associated_token_account: Account<'info, TokenAccount>,
-
-    /// CHECK: account constraint checked in account trait
-    #[account(address = mpl_token_metadata::id())]
-    pub token_metadata_program: UncheckedAccount<'info>,
-
-    pub token_program: Program<'info, Token>,
-    pub rent: Sysvar<'info, Rent>,
-    pub associated_token_program: Program<'info, AssociatedToken>,
-    pub system_program: Program<'info, System>
-}
-
-#[derive(Accounts)]
-pub struct TransferToken<'info> 
-{
-    pub from: Signer<'info>,
-
-    #[account(mut)]
-    pub from_ata: Account<'info, TokenAccount>,
-
-    #[account(mut)]
-    pub to_ata: Account<'info, TokenAccount>,
-
-    pub token_program: Program<'info, Token>
 }
